0ef1925d0a82c707205ba58fb0e34c2db070da5d
Gabriel Fernando <gf313074@gmail.com>
2025-07-18 13:58:35 -0300

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+export default tseslint.config(
+  { ignores: ['build', 'node_modules'] },
+
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      eslintConfigPrettier,
+    ],
+    files: ['src/**/*.{ts,js}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: { ...globals.nodeBuiltin },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+);
