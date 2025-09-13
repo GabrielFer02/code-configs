5b4b0c07b07cd3be1fab191cc4bf776480f8d5f5
Gabriel Fernando <gf313074@gmail.com>
2025-09-13 07:52:23 -0300

    alter configs

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import vitest from '@vitest/eslint-plugin';
 
 export default tseslint.config(
   { ignores: ['build', 'node_modules'] },
@@ -13,11 +14,11 @@ export default tseslint.config(
       ...tseslint.configs.strictTypeChecked,
       eslintConfigPrettier,
     ],
-    files: ['src/**/*.{ts,js}'],
+    files: ['src/**/*.{ts,js}', 'src/**/*.spec.{ts,js}'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
-      globals: { ...globals.nodeBuiltin },
+      globals: { ...globals.nodeBuiltin, ...vitest.environments.env.globals },
       parserOptions: {
         project: true,
         tsconfigRootDir: import.meta.dirname,
@@ -25,10 +26,13 @@ export default tseslint.config(
     },
     plugins: {
       'simple-import-sort': simpleImportSort,
+      vitest,
     },
+
     rules: {
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 );
